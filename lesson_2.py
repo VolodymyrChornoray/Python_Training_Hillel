0b59aa2966e508ca7c7da121dcf3737c06cfe87c
vladimir <chernomor.vc@gmail.com>
2022-03-22 11:25:39 +0200

    Python_lesson2(created)

---
+++ lesson_2.py
@@ -0,0 +1,17 @@
+print(1 + 2 + 3)
+value = "1*2*3"
+print(value)
+value = "1" + "2" + "3"
+print(value)
+value = "1" + "2" * 3
+print(value)
+value = ("1" + "2") * 3
+print(value)
+value_1 = 2
+value_2 = 4
+result = value_1 * value_2
+print(result)
+value_1 = "2"
+value_2 = "4"
+result = value_1 + value_2
+print(result)
