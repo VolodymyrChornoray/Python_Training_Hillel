23f4589435081d6623e0b3a7d0635e0f2f9e9786
vladimir <chernomor.vc@gmail.com>
2022-04-29 11:30:48 +0300

    Homework_11_revised

--- Homework_11.py
+++ Homework_11.py
@@ -1,10 +1,9 @@
 import os
-from typing import Tuple, Dict, Any
 
 DEBUG_MODE = True
 
 
-def return_dict_name(path) -> dict:
+def return_dict_name(path: str) -> dict:
     list_dir = os.listdir(path)
     for filename in list_dir:
         filepath = os.path.join(path, filename)
@@ -18,39 +17,25 @@ def return_dict_name(path) -> dict:
     return dict_name
 
 
-def return_reverse_dict(result_1, debug_mode=DEBUG_MODE) -> dict:
-    global files_list, dirs_list
-    for key, value in result_1.items():
-        if key == 'filenames':
-            files_list = value
-        if key == 'dirnames':
-            dirs_list = value
+def return_reverse_dict(result_1: dict, debug_mode=DEBUG_MODE) -> dict:
     if debug_mode:
-        files_dirs_dict_sort = {'filenames': sorted(files_list),
-                                'dirnames': sorted(dirs_list),
+        files_dirs_dict_sort = {'filenames': sorted(result_1['filenames']),
+                                'dirnames': sorted(result_1['dirnames']),
                                 }
     else:
-        files_dirs_dict_sort = {'filenames': sorted(files_list, reverse=True),
-                                'dirnames': sorted(dirs_list, reverse=True),
+        files_dirs_dict_sort = {'filenames': sorted(result_1['filenames'], reverse=True),
+                                'dirnames': sorted(result_1['dirnames'], reverse=True),
                                 }
     return files_dirs_dict_sort
 
 
-def add_files_dirs(result_1: dict, file_name: str) -> Tuple[Dict[str, Any], str]:
-    global files_list, dirs_list
-    for key, value in result_1.items():
-        if key == 'filenames':
-            files_list = value
-        if key == 'dirnames':
-            dirs_list = value
-    if os.path.isfile(file_name):
-        files_list.append(file_name)
-    if os.path.isdir(file_name):
-        dirs_list.append(file_name)
-    add_new_files_dirs = {'filenames': files_list,
-                          'dirnames': dirs_list,
-                          }
-    return add_new_files_dirs, file_name
+def add_files_dirs(result_1: dict, file_name: str) -> dict:
+    if '.' in file_name:
+        add_new_files_dirs = {'filenames': result_1['filenames'] + [file_name]}
+    else:
+        add_new_files_dirs = {'dirnames': result_1['dirnames'] + [file_name]}
+
+    return add_new_files_dirs
 
 
 path = 'test_dir'
